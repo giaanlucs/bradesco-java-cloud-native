1ee4eacc15626abcab5179fcd51be9dff911e562
Gianluca Correa <167277347+giaanlucs@users.noreply.github.com>
2025-02-25 20:11:19 +0000

    Adicionando CaixaEletronico.java

---
+++ CaixaEletronico.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class CaixaEletronico {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double saldo = 550.0;           
+
+        System.out.println("Digite o valor a ser sacado: ");
+        double valorSolicitado = scanner.nextDouble();
+
+
+        if (valorSolicitado <= saldo) { 
+        saldo = saldo - valorSolicitado;
+        System.out.println("Saque realizado com sucesso!");
+        System.out.println("O saldo atual é: " + saldo);
+    } else {
+        System.out.println("O saldo atual não é suficiente");
+    }
+    scanner.close();
+}
+}
